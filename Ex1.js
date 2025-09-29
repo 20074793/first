da6da290c0813688eef06b1adbee9f023b8a8291
20074793 <20074793@mydbs.ie>
2025-09-29 12:02:57 +0100

    change function

--- Ex1.js
+++
@@ -1,8 +0,0 @@
-function checkBaggageWeight(weight) {
-  const limit = 15; // weight limit in kg
-  if (weight > limit) {
-    alert("⚠️ Your baggage is overweight! The limit is " + limit + "kg.");
-  } else {
-    alert("✅ Your baggage is within the allowed limit.");
-  }
-}
