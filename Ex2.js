8876cb3a2c2ed7400b00ddb3bfb5ac9878b6718a
20074793 <20074793@mydbs.ie>
2025-10-01 10:19:34 +0100

    ex2 changes

--- Ex2.js
+++ Ex2.js
@@ -1,14 +1,14 @@
 // Standard Project Euler Problem 1 (3 or 5 below 1000)
 
-//function euler1() {
-///  let sum = 0;
-///  for (let i = 1; i < 1000; i++) {
-  ///  if (i % 3 === 0 || i % 5 === 0) {
- ///     sum += i;
-   /// }
-///  }
- /// alert("Sum of multiples of 3 or 5 below 1000 is: " + sum);
-///}
+function euler1() {
+ let sum = 0;
+for (let i = 1; i < 1000; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  alert("Sum of multiples of 3 or 5 below 1000 is: " + sum);
+}
 
 
 // Custom multiples with user input A, B, N
