81e3ea8e44c6a206eaf0c15a8535d0f9637303f2
20074793 <20074793@mydbs.ie>
2025-10-06 09:15:22 +0100

    new

--- Ex3.js
+++ Ex3.js
@@ -1,9 +1,8 @@
 
-// First version (two numbers a and b)
 function eulerlist() {
   const a = parseInt(document.getElementById("a1").value);
   const b = parseInt(document.getElementById("b1").value);
-  const l = document.getElementById("l").value.split(",").map(Number);
+  const l = document.getElementById("l").value.split(" ").map(Number);
 
   const sum = l.filter(x => x % a === 0 || x % b === 0)
                .reduce((acc, x) => acc + x, 0);
@@ -11,10 +10,10 @@ function eulerlist() {
   alert("Sum = " + sum);
 }
 
-// Second version (list of divisors and list of numbers)
+
 function euler2Lists() {
-  const aList = document.getElementById("aList").value.split(",").map(Number);
-  const mList = document.getElementById("mList").value.split(",").map(Number);
+  const aList = document.getElementById("aList").value.split(" ").map(Number);
+  const mList = document.getElementById("mList").value.split(" ").map(Number);
 
   const sum = mList.filter(x => aList.some(a => x % a === 0))
                    .reduce((acc, x) => acc + x, 0);
@@ -22,17 +21,16 @@ function euler2Lists() {
   alert("Sum = " + sum);
 }
 
-// Maybe another variation for testing
 function euler2Lists1() {
-  const aList = document.getElementById("aList").value.split(",").map(Number);
-  const mList = document.getElementById("mList").value.split(",").map(Number);
+  const aList = document.getElementById("aList").value.split(" ").map(Number);
+  const mList = document.getElementById("mList").value.split(" ").map(Number);
 
   let sum = 0;
   for (let x of mList) {
     for (let a of aList) {
       if (x % a === 0) {
         sum += x;
-        break; // prevent double counting
+        break;
       }
     }
   }
